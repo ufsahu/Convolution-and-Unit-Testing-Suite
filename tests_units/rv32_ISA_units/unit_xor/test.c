c6b54020d08cf2545b006d2b6c1af9e099db11a4
Uruj Sarwar <63293023+ufsahu@users.noreply.github.com>
2022-01-20 09:40:20 +0200

    Update test.c

--- tests_units/rv32_ISA_units/unit_xor/test.c
+++ tests_units/rv32_ISA_units/unit_xor/test.c
@@ -7,23 +7,6 @@
 //verifying variable values before and after Inline asm block 
 
 
-void print32_hex(uint32_t number)
-
-{
-  int k;
-  int ch;
-
-  for (k = 0; k < 8; k++)
-  {
-    ch = (number & 0xF0000000) >> 28; // Shift most significant 4 bits to low 4 bits
-    if (ch < 10)
-      ch = ch + '0';                  // If ch = 0 - 9, add ascii '0'
-    else
-      ch = ch + 'A' - 10;             // If ch = A - F, add ascii 'A' - 10
-    putchar (ch);
-    number = number << 4;          // Shift next 4 bits to MS bits
-  }
-}
 
 
 
@@ -54,9 +37,7 @@ int main()
   printf("Input value_1 =  0x00000000\n");
   printf("Input value_2 =  0x00000001\n");
   printf("XOR operation performed on above values and stored in variable var_xor \n");
-  printf("Printing Output var_xor \n");
-  print32_hex(var_xor);
-  printf("\n"); 
+ 
 
 if(var_xor==1){
 	printf("Pass for xor instruction var_xor == 0x00000001\n");
