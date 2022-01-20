e9d0d77891e1a0bd79ccbf1bed8dbd1b0d2095b5
Uruj Sarwar <63293023+ufsahu@users.noreply.github.com>
2022-01-20 09:37:37 +0200

    Update test.c

--- tests_units/rv32_ISA_units/unit_and/test.c
+++ tests_units/rv32_ISA_units/unit_and/test.c
@@ -9,23 +9,7 @@
 
 
 
-void print32_hex(uint32_t number)
 
-{
-  int k;
-  int ch;
-
-  for (k = 0; k < 8; k++)
-  {
-    ch = (number & 0xF0000000) >> 28; // Shift most significant 4 bits to low 4 bits
-    if (ch < 10)
-      ch = ch + '0';                  // If ch = 0 - 9, add ascii '0'
-    else
-      ch = ch + 'A' - 10;             // If ch = A - F, add ascii 'A' - 10
-    putchar (ch);
-    number = number << 4;          // Shift next 4 bits to MS bits
-  }
-}
 
 
 
@@ -55,9 +39,6 @@ int main()
     );
   printf("End after asm block \n");
 
-  printf("Printing Output of AND variable var_and = 0 . 1: \n");
-  print32_hex(var_and);
-  printf("\n"); 
 
 if(var_and==0){
 	printf("Pass for and instruction var_and == 0\n");
